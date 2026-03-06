d101659d5f2fee23330ea65437759ad9adaccf7e
Ben Joseph <benjosephcr7@gmail.com>
2026-03-06 09:23:05 +0530

    Initial Commit

---
+++ Controllers/SongBookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SongBook.API.Models.Request;
+
+namespace SongBook.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongBookController : ControllerBase
+    {
+        public SongBookController()
+        {
+        }
+
+        [HttpGet]
+        [Route("songs")]
+        public async Task<IActionResult> GetSongs(int pageNo, string searchText)
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpPost]
+        [Route("saveSong")]
+        public async Task<IActionResult> SaveSong(Song request)
+        {
+            return Ok($"Hello {request.ToString()}");
+        }
+
+    }
+}
---
+++ Models/Request/Song.cs
@@ -0,0 +1,12 @@
+﻿namespace SongBook.API.Models.Request
+{
+    public class Song
+    {
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public List<string>? Stanzas { get; set; }
+        public string? Category { get; set; }
+        public int Year { get; set; }
+        public int? StanzaNos { get; set; }
+    }
+}
